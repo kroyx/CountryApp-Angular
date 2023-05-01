018ac85c46100a2516ff589f887be2b54d9d6b05
Lelei <66555957+xLelei@users.noreply.github.com>
2023-05-01 11:51:52 +0200

    Diseño inicial del menu de navegacion

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { AboutPageComponent } from './pages/about-page/about-page.component';
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HomePageComponent} from './pages/home-page/home-page.component';
+import {AboutPageComponent} from './pages/about-page/about-page.component';
+import {SidebarComponent} from './components/sidebar/sidebar.component';
+import {RouterModule} from '@angular/router';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 
 @NgModule({
   declarations: [
     AboutPageComponent,
-    HomePageComponent
+    HomePageComponent,
+    SidebarComponent,
+    ContactPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     AboutPageComponent,
-    HomePageComponent
+    HomePageComponent,
+    SidebarComponent
   ]
 })
-export class SharedModule { }
+export class SharedModule {}
