79aaf18ced1cc0a61e843d71334d9d6dfc41d866
Lelei <66555957+xLelei@users.noreply.github.com>
2023-05-06 12:50:06 +0200

    Desarrollada funcionalidad de busqueda por capital

---
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -0,0 +1,22 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+@Component({
+  selector: 'shared-search-box',
+  templateUrl: './search-box.component.html',
+  styles: [
+  ]
+})
+export class SearchBoxComponent {
+
+  @Input()
+  public placeholder: string = '';
+
+  @Output()
+  public onValue: EventEmitter<string> = new EventEmitter<string>();
+
+  public emitValue(term: string): void {
+    this.onValue.emit(term);
+  }
+
+
+}
