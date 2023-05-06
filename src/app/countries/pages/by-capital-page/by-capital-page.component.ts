2398a84f219f13851cee35f61be338aeebbf1446
Lelei <66555957+xLelei@users.noreply.github.com>
2023-05-06 18:03:56 +0200

    Desarrollada funcionalidad de busqueda por region

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
-import {CountriesService} from '../../services/countries.service';
 import {Country} from '../../interfaces/country';
+import {CountriesService} from '../../services/countries.service';
 
 @Component({
   selector: 'countries-by-capital-page',
