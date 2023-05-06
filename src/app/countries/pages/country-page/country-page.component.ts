a45bda4e20bdea6c2e3a3300c46a35df2a752d92
Lelei <66555957+xLelei@users.noreply.github.com>
2023-05-06 20:14:33 +0200

    Desarrollada funcionalidad de detalle de un pais

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {count, switchMap} from 'rxjs';
+import {Country, Translation} from '../../interfaces/country';
+import {CountriesService} from '../../services/countries.service';
 
 @Component({
   selector: 'countries-country-page',
   templateUrl: './country-page.component.html',
-  styles: [
-  ]
+  styles: []
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
 
+  public country?: Country;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private countriesService: CountriesService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+        .pipe(
+          switchMap(({id}) => this.countriesService.searchCountryByAlphaCode(id)),
+        )
+        .subscribe(country => {
+          if (!country) return this.router.navigateByUrl('');
+          return this.country = country;
+        });
+  }
+
+  get allTranslations(): Translation[] {
+    return Object.values(this.country!.translations);
+  }
 }
