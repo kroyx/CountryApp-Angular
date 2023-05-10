b64a370b2eed97e820bd0e8b3267d9d6faad9cde
Lelei <66555957+xLelei@users.noreply.github.com>
2023-05-10 19:28:00 +0200

    Optimizacion de la busqueda por region

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,14 @@ import {Component} from '@angular/core';
 import {Country} from '../../interfaces/country';
 import {CountriesService} from '../../services/countries.service';
 
+enum Region {
+  Africa = 'Africa',
+  Americas = 'Americas',
+  Asia = 'Asia',
+  Europe = 'Europe',
+  Oceania = 'Oceania'
+}
+
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -10,16 +18,28 @@ import {CountriesService} from '../../services/countries.service';
 export class ByRegionPageComponent {
 
   public countries: Country[] = [];
+
   public isLoading: boolean = false;
+
+  public regions: Region[] = [
+    Region.Africa,
+    Region.Americas,
+    Region.Asia,
+    Region.Europe,
+    Region.Oceania
+  ];
+
+  public selectedRegion?: Region;
 
   constructor(private countriesService: CountriesService) {}
 
-  searchByRegion(term: string): void {
+  searchByRegion(region: Region): void {
+    this.selectedRegion = region;
     this.isLoading = true;
-    this.countriesService.searchRegion(term)
-        .subscribe(countries => {
-          this.countries = countries;
-          this.isLoading = false;
-        });
+    this.countriesService.searchRegion(region)
+      .subscribe(countries => {
+        this.countries = countries;
+        this.isLoading = false;
+      });
   }
 }
